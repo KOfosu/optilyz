8d6c632e0d59da48ca0ef69269b921cd39009398
kofosu <kingsleyofosu@yahoo.com>
2021-10-08 17:09:51 +0000

    update before install and after install scripts

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,10 +2,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-#cp /home/ubuntu/.env /home/ubuntu/apps/valentines-payment/
-#sudo apt install npm -y
-#npm install -g @nestjs/cli
-
 cd /var/www/apps/optilyz
 npm i
 pm2 delete optilyz
